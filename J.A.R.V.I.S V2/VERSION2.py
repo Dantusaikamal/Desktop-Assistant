b41e371398a52091055ea08dadc0d50d95e5e39f
Dantusaikamal <dantusaikamal@gmail.com>
2021-03-20 18:32:11 +0530

    Replacing files

---
+++ J.A.R.V.I.S V2/VERSION2.py
@@ -0,0 +1,141 @@
+
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+from weather import Weather
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Jarvis Sir. Please tell me how may I help you")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('tiny.gamer@gmail.com', 'angryfight')
+    server.sendmail('tiny.gamer6@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            speak('Opening Youtube, Sir')
+            webbrowser.open("youtube.com")
+
+        elif 'open Gmail' in query:
+            speak('Just a minute, sir. Opening Gmail')
+            webbrowser.open("Gmail.com")
+
+        elif 'open google' in query:
+            speak('Okay. Opening Google')
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            speak('Opening Stackoverflow')
+            webbrowser.open("stackoverflow.com")   
+
+
+        elif 'play music' in query:
+            speak('Playing your favourite music')
+            music_dir = r'C:\Users\dantu\Music\Music'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'open website' in takeCommand():
+            reg_ex = re.search('open website (.+)', takeCommand())
+            if reg_ex:
+                domain = reg_ex.group(1)
+                url = 'https://www.' + domain
+                webbrowser.open(url)
+                print('Done!')
+            else:
+                pass
+
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            speak('Opening VS Code. Carry on with your programming,Sir')
+            codePath = "C:\\Users\\Dantu\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        elif 'email to kamal' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "dantusaikamal@gmail.com"
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry Sir. I am not able to send this email")
+
+
+        elif 'open Spotify' in query:
+            speak('Opening VS Code. Carry on with your programming,Sir')
+            codePath = "C:\\Users\\Dantu\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+        # elif 'Jarvis sleep' in query:
+        #     speak('Wake me up, when needed Sir')
+        #     exit(1)
